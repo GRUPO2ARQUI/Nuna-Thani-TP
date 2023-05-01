dd34a2f74837f8815eaeac4d60a533e58d535a44
Ibeth010 <81484018+Ibeth010@users.noreply.github.com>
2023-05-01 05:04:06 -0500

    Update app-routing.module.ts

--- demo01/src/app/app-routing.module.ts
+++ demo01/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { EspecialidadComponent } from './component/especialidad/especialidad.component';
 import { EspecialidadInsertarComponent } from './component/especialidad/especialidad-insertar/especialidad-insertar.component';
 
+import { EstadoInsertarComponent } from './component/estado/estado-insertar/estado-insertar.component';
+import { EstadoComponent } from './component/estado/estado.component';
+
 const routes: Routes = [
   {
     path: 'especialidad',
@@ -11,11 +14,23 @@ const routes: Routes = [
       {
         path: 'especialidadinsertar',component: EspecialidadInsertarComponent},
       {path: 'edicion/:id',component: EspecialidadInsertarComponent},
-
     ],
-
-
   },
+  
+  {
+    path: 'estado',component: EstadoComponent,children: [
+      {path: 'estadoinsertar', component: EstadoInsertarComponent},
+      {path: 'edicion/:id', component: EstadoInsertarComponent},
+    ],
+  },
+  
+  
+  
+  
+  
+  
+  
+  
 ];
 
 @NgModule({
